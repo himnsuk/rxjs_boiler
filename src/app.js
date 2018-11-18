2a547ae2ec439bf9e5f368a3c4415540228e1867
Himanshu <himnsuk@gmail.com>
2018-11-18 09:14:04 +0530

    Added starting commit with rxjs example

--- src/app.js
+++ src/app.js
@@ -1,4 +1,14 @@
 import $ from 'jquery';
 import Rx from 'rxjs/Rx';
 
-console.log('RxJS Boiler Running...');
+const btn = $('#btn');
+
+const btnStream$ = Rx.Observable.fromEvent(btn, 'click');
+
+btnStream$.subscribe((e) => {
+  console.log('Clicked');
+}, (error) => {
+  console.log(error);
+}, () => {
+  console.log('Completed')
+})
